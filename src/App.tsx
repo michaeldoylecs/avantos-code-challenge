8eb4e007d8f6484c758f119b4897b479804166d3
Michael Doyle <mcdoyle7227@gmail.com>
2025-07-02 17:34:11 -0400

    Add zod schema type validation to API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import * as z from "zod/v4"
 import './App.css'
 
+const SLADuration = z.object({
+  number: z.number(),
+  unit: z.string(),
+})
+
+const Position = z.object({
+  x: z.number(),
+  y: z.number(),
+})
+
+const Form = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  is_reusable: z.boolean(),
+  field_schema: z.object(), // Left untyped
+})
+
+const Edge = z.object({
+  source: z.string(),
+  target: z.string(),
+})
+
+const NodeData = z.object({
+  id: z.string(),
+  component_key: z.string(),
+  component_type: z.string(),
+  component_id: z.string(),
+  name: z.string(),
+  prerequisites: z.array(z.string()),
+  permitted_roles: z.array(z.string()),
+  input_mapping: z.object(),
+  sla_duration: SLADuration,
+  approval_required: z.boolean(),
+  approval_roles: z.array(z.string()),
+})
+
+const Node = z.object({
+  id: z.string(),
+  type: z.string(),
+  position: Position,
+  data: NodeData,
+})
+
+const ActionBlueprintGraphSchema = z.object({
+  "$schema": z.string(),
+  id: z.string(),
+  tenant_id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  category: z.string(),
+  nodes: z.array(Node),
+  edges: z.array(Edge),
+  forms: z.array(Form),
+  branches: z.array(z.string()),
+  triggers: z.array(z.string()),
+})
+
+type ActionBlueprintGraph = z.infer<typeof ActionBlueprintGraphSchema>
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<ActionBlueprintGraph>()
+  
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/1/actions/blueprints/1/graph/")
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error(`HTTP error: ${response.status}`)
+        }
+      })
+      .then(data => {
+        return ActionBlueprintGraphSchema.parse(data)
+      })
+      .then(data => {
+        setData(data)
+      })
+  }, [])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <code>
+          {JSON.stringify(data, null, 2)}
+        </code>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
